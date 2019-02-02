65c1105a73b6498e4b052577979a641a61ea305e
wailunlim <wailun@u.nus.edu>
2019-02-02 14:57:29 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about' # automatically create helper called static_pages_about_url
+
   root 'application#hello'
 end
