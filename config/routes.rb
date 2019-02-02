9f0e521ee1923f90e87055a6f00e3c965a0c073e
wailunlim <wailun@u.nus.edu>
2019-02-02 16:01:59 +0800

    set root route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about' # automatically create helper called static_pages_about_url
-
-  get 'static_pages/contact'
-
-  root 'application#hello'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
 end
