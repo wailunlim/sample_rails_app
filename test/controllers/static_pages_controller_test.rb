0f0baf9db672f32d81e88dc5b292341e5eba4dcd
wailunlim <wailun@u.nus.edu>
2019-02-05 17:49:00 +0800

    add test for title of contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -31,5 +31,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
